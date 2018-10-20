5648bb45b7e7359be0d39f361249eca62a0b15ba
Naresh <nmanthena18@gmail.com>
2018-10-21 00:19:09 +0530

    i

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
+// const express = require('express');
+// const app = express();
+// const path = require('path');
+// app.use(express.static('./dist/iShop'));
+// app.get('/*', function(req, res) {
+//   res.sendFile(path.join('./dist/iShop/index.html'));
+// });
+// app.listen(process.env.PORT || 8080);
+
 const express = require('express');
 const app = express();
 const path = require('path');
+
 app.use(express.static('./dist/iShop'));
 app.get('/*', function(req, res) {
   res.sendFile(path.join('./dist/iShop/index.html'));
 });
-app.listen(process.env.PORT || 8080);
+
+app.listen(process.env.PORT || 4000, function(){
+    console.log('Your node js server is running');
+});
