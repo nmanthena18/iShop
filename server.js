74043c51004313a37a6fd3011b855bf5ecfdcf60
Naresh <nmanthena18@gmail.com>
2018-10-20 23:16:34 +0530

    hello

--- server.js
+++ server.js
@@ -1,16 +1,8 @@
-//Install express server
 const express = require('express');
-const path = require('path');
-
 const app = express();
-
-// Serve only the static files form the dist directory
+const path = require('path');
 app.use(express.static('./dist/iShop'));
-
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname,'/dist/iShop/index.html'));
+app.get('/*', function(req, res) {
+  res.sendFile(path.join('./dist/iShop/index.html'));
 });
-
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
