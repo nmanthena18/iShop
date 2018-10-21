97cff8734237c0f9e39fd09c2b65b97ea2d45a53
Naresh <nmanthena18@gmail.com>
2018-10-21 07:38:07 +0530

    prepare heroku

--- server.js
+++ server.js
@@ -11,11 +11,11 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static('./dist/iShop'));
+app.use(express.static(__dirname, './dist'));
 app.get('/*', function(req, res) {
-  res.sendFile(path.join('./dist/iShop/index.html'));
+  res.sendFile(path.join(__dirname, './dist/index.html'));
 });
 
-app.listen(process.env.PORT || 4000, function(){
+app.listen(process.env.PORT || 8080, function(){
     console.log('Your node js server is running');
 });
